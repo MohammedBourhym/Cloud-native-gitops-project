fbcf27f5b864a68306840d689159f571ae6bfa3c
MohammedBourhym <mohammedbourhym9@gmail.com>
2025-07-01 17:16:19 +0100

    fix tf dependencies versions

--- infra/terraform/terraform.tf
+++ infra/terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.43.0"
     }
 
-    random = {
+   random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3.6.2"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = "~> 4.0.5"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.0"
+      version = "~> 2.3.5"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "~> 2.27.0"
     }
   }
 
@@ -32,5 +32,5 @@ terraform {
     region = "us-east-2"
   }
 
-  required_version = ">= 1.0"
+  required_version = "~> 1.9.2"
 }
