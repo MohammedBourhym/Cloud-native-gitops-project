2d4d63347e415bdba62299c7819d0c76d02dbf7f
MohammedBourhym <mohammedbourhym9@gmail.com>
2025-07-01 22:26:14 +0100

    Fix workflow issues

--- infra/terraform/eks-cluster.tf
+++ infra/terraform/eks-cluster.tf
@@ -4,12 +4,12 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   cluster_version = "1.30"
-  
+
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
-  
-  
+
+
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
 
